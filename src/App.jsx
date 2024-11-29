3f85ed5dcd748fde0ae974e6af24112a77a79ae4
juniorpinhodev <juniorpinhodev@gmail.com>
2024-11-29 14:32:24 -0300

    feat: 🎸 Cria base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Sobre from './components/Sobre';
+import Games from './components/Games';
+import Footer from './components/Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Home />
+        <Sobre />
+        <Games />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
