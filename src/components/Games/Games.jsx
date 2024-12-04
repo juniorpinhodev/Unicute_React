bb713377136771e6232b0b93a064c4b8f1914743
juniorpinhodev <juniorpinhodev@gmail.com>
2024-12-04 18:27:00 -0300

    fix: 🐛 Component Games

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "../../index.css"; 
 
 const Games = () => {
-
   return (
     <section className="games" id="Games">
-      <h2 className="heading">Games <span>Unicute Relax</span></h2>   
+      <h2 className="heading">
+        Games <span>Unicute Relax</span>
+          
+
+       
+      </h2>
     </section>
   );
-}
+};
 
 export default Games;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "../../index.css";
 function Header() {
   const [isPopupOpen, setPopupOpen] = useState(false);
   const [isPlaying, setPlaying] = useState(false);
-  const [popupPosition, setPopupPosition] = useState({ top: "50%", left: "50%" });
+  const [popupPosition, setPopupPosition] = useState({ top: "65%", left: "72%" });
   const popupRef = useRef(null); // Referência para o pop-up
 
   const handlePlayPause = () => {
@@ -53,100 +53,96 @@ function Header() {
 
   return (
     <>
-      <header className="header">
-        <a href="/" className="logo">
-          UNICUTE<span>.</span>
-        </a>
+            <header className="header">
+              <a href="/" className="logo">
+                UNICUTE<span>.</span>
+              </a>
 
-        <nav className="navbar">
-          <a href="#Home" className="ativado">Home</a>
-          <a href="#Sobre">Sobre</a>
-          <a href="#Games">Games</a>
-        </nav>
+              <nav className="navbar">
+                <a href="#Home" className="ativado">Home</a>
+                <a href="#Sobre">Sobre</a>
+                <a href="#Games">Games</a>
+              </nav>
 
-                {/* Player */}
-                <div className="player">
-          <button onClick={handlePlayPause}>
-            {isPlaying ? "Pause" : "Play"}
-          </button>
+                      {/* Player */}
+                      <div className="player">
+                <button onClick={handlePlayPause}>
+                  {isPlaying ? "Pause" : "Play"}
+                </button>
+              </div>
+              
+
+              <div className="links">
+                <a
+                  href="https://www.youtube.com/@unicuterelax"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-youtube"></i>
+                </a>
+                <a
+                  href="https://www.instagram.com/unicuterelax"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-instagram-alt"></i>
+                </a>
+                <a
+                  href="https://www.tiktok.com/@unicuterelax"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-tiktok"></i>
+                </a>
+                <a
+                  href="https://www.facebook.com/unicuterelax"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="bx bxl-facebook"></i>
+                </a>
+              </div>      
+              
+              <a href="/languages/english.html" id="change-language">
+                English Language
+                <img src="src/assets/usa.png" alt="Switch to English" />
+              </a>
+
+              <div className="bx bx-menu" id="menu-icon"></div>
+
+              
+            </header>
+
+            {/* Pop-up para exibir a live */}
+            {isPopupOpen && (
+        <div
+          className={`popup ${isMinimized ? "minimized" : ""}`}
+          style={{
+            top: !isMinimized ? popupPosition.top : "auto",
+            left: !isMinimized ? popupPosition.left : "auto",
+          }}
+          ref={popupRef}
+          onMouseDown={!isMinimized ? handleMouseDown : null}
+        >
+          <div className="popup-content">
+            <button className="close-button" onClick={handleClosePopup}>
+              Fechar
+            </button>
+            <button className="minimize-button" onClick={handleMinimize}>
+              {isMinimized ? "⬆" : "⬇"}
+            </button>
+            <iframe
+              width={isMinimized ? "200" : "560"}
+              height={isMinimized ? "112" : "315"}
+              src={`https://www.youtube.com/embed/UdRLmVufx88?autoplay=${isPlaying ? 1 : 0}`}
+              title="UniCute Relax Live"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
-        
-
-        <div className="links">
-          <a
-            href="https://www.youtube.com/@unicuterelax"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bx bxl-youtube"></i>
-          </a>
-          <a
-            href="https://www.instagram.com/unicuterelax"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bx bxl-instagram-alt"></i>
-          </a>
-          <a
-            href="https://www.tiktok.com/@unicuterelax"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bx bxl-tiktok"></i>
-          </a>
-          <a
-            href="https://www.facebook.com/unicuterelax"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="bx bxl-facebook"></i>
-          </a>
-        </div>      
-        
-        <a href="/languages/english.html" id="change-language">
-          English Language
-          <img src="src/assets/usa.png" alt="Switch to English" />
-        </a>
-
-        <div className="bx bx-menu" id="menu-icon"></div>
-
-        
-      </header>
-
-      {/* Pop-up para exibir a live */}
-      {isPopupOpen && (
-  <div
-    className={`popup ${isMinimized ? "minimized" : ""}`}
-    style={{
-      top: !isMinimized ? popupPosition.top : "auto",
-      left: !isMinimized ? popupPosition.left : "auto",
-    }}
-    ref={popupRef}
-    onMouseDown={!isMinimized ? handleMouseDown : null}
-  >
-    <div className="popup-content">
-      <button className="close-button" onClick={handleClosePopup}>
-        Fechar
-      </button>
-      <button className="minimize-button" onClick={handleMinimize}>
-        {isMinimized ? "⬆" : "⬇"}
-      </button>
-      <iframe
-        width={isMinimized ? "200" : "560"}
-        height={isMinimized ? "112" : "315"}
-        src={`https://www.youtube.com/embed/UdRLmVufx88?autoplay=${isPlaying ? 1 : 0}`}
-        title="UniCute Relax Live"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    </div>
-  </div>
-)}
-
-
-
-
+      )}
     </>
   );
 }
