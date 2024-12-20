de9797a8538fa55b3d89b87ecd6bc7396a44ae41
juniorpinhodev <juniorpinhodev@gmail.com>
2024-12-20 14:13:17 -0300

    feat: 🎸 Edita texto Sobre

--- src/components/Sobre/Sobre.jsx
+++ src/components/Sobre/Sobre.jsx
@@ -18,38 +18,69 @@ const Sobre = () => {
         </div>
         
         <div className={styles['about-content']}>
-          <p>
-            Com <strong>UniCute</strong>, embarque em uma jornada encantadora de música <em>LoFi</em> e serenidade.
-            Com o nosso adorável unicórnio, UniCute, como guia, oferecemos uma live 24/7 com músicas relaxantes
-            para te acompanhar em seus <em>estudos</em>, <em>trabalhos</em>, <em>descanso</em> ou em qualquer momento do dia.
-            Além das melodias suaves, explore <em>jogos</em> relaxantes e <em>dicas anti-stress</em> para cuidar do seu <em>bem-estar</em> <em>mental</em> e <em>emocional</em>.
-          
-            Este é o lugar perfeito para se desconectar do caos e se conectar consigo mesmo.
-            Sintonize UniCute Relax e permita-se encontrar calma e inspiração em meio ao ritmo acelerado da vida.
-            Junte-se a nós nesta jornada de tranquilidade e <em>concentração</em>, e descubra um universo de
-            <em>relaxamento</em> e harmonia.
-          </p>
+        <div className={styles['about-content']}>
+        <div className={styles['about-content']}>
 
-          <p>
-            Além disso, oferecemos <strong>jogos relaxantes</strong> especialmente desenvolvidos para proporcionar diversão 
-            enquanto você alivia o estresse. Perfeito para quem busca momentos de descontração no dia a dia!
-          </p>
+<p>                                                 
+  Seja bem-vindo ao <strong>UniCute Relax</strong>, o cantinho perfeito do <strong>LoFi Hip-Hop</strong> pra quem curte <em>vibes</em> tranquilas e quer dar uma pausa no ritmo agitado do dia a dia. Conecte-se com um universo de batidas suaves e melodias relaxantes que funcionam como trilha sonora perfeita para qualquer situação: <em>estudar</em>, <em>trabalhar</em>, <em>realizar tarefas</em> ou simplesmente relaxar. Seja para manter o foco ou aliviar o estresse, o UniCute Relax está aqui para acompanhar você.
+</p>
 
-          <p>
-            Estamos animados em anunciar que, em breve, nossas músicas estarão disponíveis nas principais plataformas de streaming, 
-            como <strong>Spotify</strong>, <strong>Deezer</strong>, <strong>Amazon Music</strong>, <strong>YouTube Music</strong>, e <strong>Apple Music</strong>. 
-            Assim, você poderá levar o universo de UniCute Relax para onde quiser!
-          </p>
+<p>
+  Com nossas <strong>Lives 24 horas</strong> e <strong>playlists LoFi</strong>, você encontrará um ambiente aconchegante sempre à disposição. Basta dar o <strong>play</strong>, desacelerar, respirar fundo e deixar tudo fluir. Encontre o equilíbrio e a tranquilidade que você merece.
+</p>
+
+<p>
+  O UniCute Relax é para todos! Se você precisa de um espaço acolhedor, UniCute Relax também é indicado para lidar com <strong>neurodivergências</strong>, como <strong>TDAH</strong> ou <strong>autismo</strong>, este é o lugar certo. Seja para organizar suas ideias, focar ou simplesmente descansar, nosso objetivo é tornar sua rotina mais leve e agradável.
+</p>
+
+<p>
+  E tem mais! Oferecemos <strong>jogos relaxantes</strong> no estilo puzzle, criados especialmente para proporcionar diversão, treinar sua memória e atenção. Além disso, estamos desenvolvendo novos jogos, blogs e outros conteúdos para tornar sua experiência ainda mais envolvente e interessante.
+</p>
+
+<p>
+  <em>Quer levar as músicas do <strong>UniCute Relax</strong> para onde você for?</em> Elas estão disponíveis nas principais plataformas de streaming, como <strong>Spotify</strong>, <strong>Deezer</strong>, <strong>Amazon Music</strong>, <strong>YouTube Music</strong>, <strong>TikTok</strong>, <strong>Apple Music</strong>, entre outras. Basta escolher sua favorita e aproveitar!
+</p>
+
+<p>
+  Permita-se explorar o universo do UniCute Relax. Deixe que nossas batidas e melodias transformem o seu dia em uma experiência única de tranquilidade e inspiração.
+</p>
+
+
+</div>
+
+</div>
+
 
           {/* Carrossel de Logos */}
           <div className={styles['streaming-carousel']}>
-            <h3 className={styles['carousel-heading']}>Disponível em breve:</h3>
+            <h3 className={styles['carousel-heading']}>Disponível nas principais plataformas de Streamings:</h3>
             <div className={styles['carousel-icons']}>
-              <i className="bx bxl-spotify" aria-label="Spotify"></i>
+              {/* <i className="bx bxl-spotify" aria-label="Spotify"></i>
               <i className="bx bxl-deezer" aria-label="Deezer"></i>
               <i className="bx bxl-amazon" aria-label="Amazon Music"></i>
-              <i className="bx bxl-tiktok" aria-label="Tik Tok"></i>
               <i className="bx bxl-youtube" aria-label="YouTube Music"></i>
+              <i className="bx bxl-tiktok" aria-label="Tik Tok"></i>
+              <i className="bx bxl-apple" aria-label="Apple Music"></i> */}
+
+
+              <a href="https://open.spotify.com/intl-pt/artist/15rAb41co5784UdtRFGRWD" target="_blank" rel="noopener noreferrer" >
+                  <i className="bx bxl-spotify" aria-label="Spotify"></i>
+                </a>
+                <a aria-label="Deezer">
+                  <i className="bx bxl-deezer"></i>
+                </a>
+                <a href="https://music.amazon.com/artists/B0DQVJ5GS1/unicute-relax" target="_blank" rel="noopener noreferrer" aria-label="Amazon Music">
+                  <i className="bx bxl-amazon"></i>
+                </a>
+                <a href="https://music.youtube.com/playlist?list=OLAK5uy_lhNClDwOZMxaUhAV938QjV4ozwgvpOvJ8" target="_blank" rel="noopener noreferrer" aria-label="YouTube Music">
+                  <i className="bx bxl-youtube"></i>
+                </a>
+                <a href="https://www.tiktok.com/@unicuterelax" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
+                  <i className="bx bxl-tiktok"></i>
+                </a>
+                <a aria-label="Apple Music"> <i className="bx bxl-apple"></i>
+                </a>
+
             </div>
           </div>
         </div>
